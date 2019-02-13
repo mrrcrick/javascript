5ea57368f269356b28d0dbd66ce8ffa955f65bbe
mrcrick <mrrcrick@gmail.com>
2019-02-13 14:37:51 +0000

    added spell check of words, using an array

--- wordGameEventListeners.js
+++ wordGameEventListeners.js
@@ -1,6 +1,14 @@
 
 let wordSubmitButton = document.querySelector('#submitWordButton');
+// attach event listener to submit word button
 wordSubmitButton.addEventListener("click",function(){
 	let word = document.querySelector('#wordBox').value;
-	document.querySelector('#scoreBoard').innerHTML = "Points: "+wordScoreCalculator(word);
+	// if word is in the word dictionary allocate points
+	if (wordList.includes(word.toLowerCase())){
+			 document.querySelector('#scoreBoard').innerHTML = "Points: "+wordScoreCalculator(word);
+
+	}else
+	{
+		 document.querySelector('#scoreBoard').innerHTML = "Incorrect word spelt!!!!";
+	}
 });
